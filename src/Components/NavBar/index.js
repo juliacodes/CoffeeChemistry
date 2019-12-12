c8b9fa0916393f0d746ecc98430c776ac35fa15d
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-12-12 13:31:49 -0500

    commented out unneeded calcs

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -16,20 +16,21 @@ export default class Navbar extends React.Component {
     }
 
     handleScroll() {
-        const windowHeight =
-            'innerHeight' in window
-                ? window.innerHeight
-                : document.documentElement.offsetHeight;
-        const { body } = document;
-        const html = document.documentElement;
-        const docHeight = Math.max(
-            body.scrollHeight,
-            body.offsetHeight,
-            html.clientHeight,
-            html.scrollHeight,
-            html.offsetHeight
-        );
-        const windowBottom = windowHeight + window.pageYOffset;
+        // ADD IF YOU NEED BOTTOM
+        // const windowHeight =
+        //     'innerHeight' in window
+        //         ? window.innerHeight
+        //         : document.documentElement.offsetHeight;
+        // const { body } = document;
+        // const html = document.documentElement;
+        // const docHeight = Math.max(
+        //     body.scrollHeight,
+        //     body.offsetHeight,
+        //     html.clientHeight,
+        //     html.scrollHeight,
+        //     html.offsetHeight
+        // );
+        // const windowBottom = windowHeight + window.pageYOffset;
 
         if (window.pageYOffset <= 50) {
             this.setState({
