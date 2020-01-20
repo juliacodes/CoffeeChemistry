0077306fccdcb078331af2637d815a96ab65deea
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2020-01-19 19:05:14 -0500

    Added remaining coffee type pages

---
+++ src/Views/Aeropress.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Inner } from '../Styling';
+import TypeContent from '../Components/RecipePage/TypeContent';
+import NavBar from '../Components/NavBar';
+
+function Aeropress() {
+    return (
+        <Container>
+            <NavBar type='simple' />
+            <Inner>
+                <TypeContent
+                    type='Aeropress'
+                    steps='aeropress'
+                    ingredients='Chemex carafe, chemex filters, grinder, digital scale, kettle'
+                    paragraph='As one of the few coffee brewers that lives in MOMA’s
+                    permanent design collection, Chemex is an iconic and
+                    beautiful brew style that creates a unique and bright
+                    flavor profile. While Chemex’s unique glass shape
+                    definitely contributes to its great flavor, its patented
+                    filter’s tight woven design is what makes this a truly
+                    unique tasting coffee.'
+                    flavor1='Fresh'
+                    flavor2='Clean'
+                />
+            </Inner>
+        </Container>
+    );
+}
+
+export default Aeropress;
---
+++ src/Views/Pourover.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Inner } from '../Styling';
+import TypeContent from '../Components/RecipePage/TypeContent';
+import NavBar from '../Components/NavBar';
+
+function Pourover() {
+    return (
+        <Container>
+            <NavBar type='simple' />
+            <Inner>
+                <TypeContent
+                    type='Pourover'
+                    steps='pourover'
+                    ingredients='French Press carafe, Burr Grinder, Digital Scale, Kettle'
+                    paragraph='As one of the few coffee brewers that lives in MOMA’s
+                    permanent design collection, Chemex is an iconic and
+                    beautiful brew style that creates a unique and bright
+                    flavor profile. While Chemex’s unique glass shape
+                    definitely contributes to its great flavor, its patented
+                    filter’s tight woven design is what makes this a truly
+                    unique tasting coffee.'
+                    flavor1='Fullbodied'
+                    flavor2='Rich'
+                />
+            </Inner>
+        </Container>
+    );
+}
+
+export default Pourover;
