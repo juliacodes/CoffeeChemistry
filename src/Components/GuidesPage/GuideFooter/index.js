d072433abf12d5430053f9654d9ca5fcd7ba3cca
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2020-01-28 21:06:02 -0500

    Added Linking

--- src/Components/GuidesPage/GuideFooter/index.js
+++ src/Components/GuidesPage/GuideFooter/index.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import { GuideFooterCont } from './styles';
 import { Inner, Paragraph } from '../../../Styling';
+import { Link } from 'react-router-dom';
 
 function GuideFooter() {
     return (
-        <GuideFooterCont>
-            <Inner>
-                <Paragraph>Next Guide</Paragraph>
-                <svg
-                    className='RightArrow'
-                    width='22'
-                    height='17'
-                    viewBox='0 0 22 17'
-                    fill='none'
-                    xmlns='http://www.w3.org/2000/svg'
-                >
-                    <path
-                        className='color'
-                        d='M11.708 1.79201L17.1486 7.23267L-8.53887e-07 7.23267L-6.323e-07 9.76733L17.1486 9.76733L11.708 15.208L13.5 17L22 8.5L13.5 -1.16425e-06L11.708 1.79201Z'
-                        fill='black'
-                    />
-                </svg>
-            </Inner>
-        </GuideFooterCont>
+        <Link to='/' style={{ width: '100%', textDecoration: 'none' }}>
+            <GuideFooterCont>
+                <Inner>
+                    <Paragraph>Next Guide</Paragraph>
+                    <svg
+                        className='RightArrow'
+                        width='22'
+                        height='17'
+                        viewBox='0 0 22 17'
+                        fill='none'
+                        xmlns='http://www.w3.org/2000/svg'
+                    >
+                        <path
+                            className='color'
+                            d='M11.708 1.79201L17.1486 7.23267L-8.53887e-07 7.23267L-6.323e-07 9.76733L17.1486 9.76733L11.708 15.208L13.5 17L22 8.5L13.5 -1.16425e-06L11.708 1.79201Z'
+                            fill='black'
+                        />
+                    </svg>
+                </Inner>
+            </GuideFooterCont>
+        </Link>
     );
 }
 
